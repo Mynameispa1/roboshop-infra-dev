12149cfe9e433b3ed62569efd3696da251e3fee8
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2024-01-25 04:48:41 -0600

    first commit

--- 05-app-alb/main.tf
+++ 05-app-alb/main.tf
@@ -1,11 +1,11 @@
-#creating app load balancer
+#1) Creating app load balancer
 
 resource "aws_lb" "app_alb" {
   name               = "${local.name}-${var.tags.Component}"
   internal           = true
   load_balancer_type = "application"
   security_groups    = [data.aws_ssm_parameter.app_alb_sg_id.value]
-  subnets            = split(",", data.aws_ssm_parameter.private_subnet_ids.value) #we need to provide two subnets while creating Load balancer at app level.access_logs {
+  subnets            = split(",", data.aws_ssm_parameter.private_subnet_ids.value) #we need to provide two subnets while creating Load balancer at app level
     
   #enable_deletion_protection = true
 
@@ -15,7 +15,7 @@ resource "aws_lb" "app_alb" {
   )
 }
 
-#listner and rules for the load balancer
+#2) Listner and rules for the load balancer
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app_alb.arn
   port              = "80"
@@ -33,7 +33,7 @@ resource "aws_lb_listener" "http" {
 }
 
 
-
+#3) We are creating DNS record because the default dns will keep change whenever we delete and create the resource  // *.app-dev.pavankumarmuvva.online //
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
 
@@ -41,7 +41,7 @@ module "records" {
 
   records = [
     {
-      name    = "*.app-${var.environment}"
+      name    = "*.app-${var.environment}"  #*.app-dev and pavankumarmuvva.online will take automatically
       type    = "A"
       alias   = {
         name    = aws_lb.app_alb.dns_name
--- 06-catalogue/main.tf
+++ 06-catalogue/main.tf
@@ -1,3 +1,13 @@
+# 1.Create target group
+#     a) Add health check block while creating the target group
+# 2. In the process of autoscalling. 
+    # a)Create one instance
+    # b)provision the instnace using the ansible
+    # c)stop the instnace.
+    # d)take the Ami
+    # e)Delete the instance
+    # f)Now, finally create launch template
+
 #Creating a target group
 resource "aws_lb_target_group" "test" {
   name     = "${local.name}-${var.tags.Component}"
@@ -12,6 +22,101 @@ resource "aws_lb_target_group" "test" {
     unhealthy_threshold = 3
     timeout = 5
     interval = 10
-    matcher = "200"  # has to be HTTP 200 or fails
+    matcher = "200-299"  # has to be HTTP 200 or fails
   }
 }
+
+#creating instance
+module "catalogue" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos.id
+  name                   = "${local.name}-${var.tags.Component}-ami"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
+  subnet_id              = element(split(",", data.aws_ssm_parameter.private_subnet_ids.value), 0)
+  tags = merge(
+    var.common_tags,
+    var.tags
+  )
+}
+
+#provisioning the instnace using the ansible
+
+resource "null_resource" "catalogue" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    instance_id = module.catalogue.id
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host = module.catalogue.private_ip
+    type = "ssh"
+    user = "centos"
+    password = "DevOps321"
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh catalogue dev"
+    ]
+  }
+}
+
+#Stopping the instance which is created 
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = module.catalogue.id
+  state       = "stopped"
+  depends_on = [ null_resource.catalogue ] #we need to mention depends_on becuase it will be stopped during the provisioning itself
+}
+
+#Taking Ami from the instance
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${local.name}-${var.tags.Component}-${local.current_time}"
+  source_instance_id = module.catalogue.id
+}
+
+#Deleting the instance which we created.
+resource "null_resource" "catalogue_delete" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    instance_id = module.catalogue.id
+  }
+
+  provisioner "local-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    command = "aws ec2 terminate-instances --instance-ids ${module.catalogue.id}"
+  }
+
+  depends_on = [ aws_ami_from_instance.catalogue]
+}
+
+#Creating Launch Templates
+resource "aws_launch_template" "catalogue" {
+  name = "${local.name}-${var.tags.Component}"
+  image_id = aws_ami_from_instance.catalogue.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "${local.name}-${var.tags.Component}"
+    }
+  }
+}
+
+
+
+
+
