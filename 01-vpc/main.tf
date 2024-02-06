a3edcd260488af878f2502286e66a34583e2ce11
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2024-02-06 02:22:46 -0600

    jenkins

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,5 +1,6 @@
 module "roboshop" {
-  source = "../../terraform/terraform-aws-vpc"
+  # source = "../../terraform/terraform-aws-vpc"
+  source = "git::https://github.com/Mynameispa1/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
